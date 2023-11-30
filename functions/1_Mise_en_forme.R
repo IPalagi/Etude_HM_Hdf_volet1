ef21a03ac9b7f19b7a5ad69bb134ecdb9809e5df
IPalagi <imane.palagi@gmail.com>
2023-11-30 16:42:30 +0100

    Correction erreur station manquante pour Qb_Q2 + ajout script etape 2 (qualité globale des métriques)

--- functions/1_Mise_en_forme.R
+++ functions/1_Mise_en_forme.R
@@ -17,19 +17,25 @@ function_Qb_Q2 <- function(code_region, Q2_fr, hydro_carhyce){
   # Q2 sites hydro (totaux)
   
   Q2_fr<-Q2_fr %>% 
-    mutate( 
+    mutate( Code_sta=as.character(Code_sta),
            Surface_BV_hydro= as.numeric(Surface_BV_hydro),
            Surface_BV_topo= as.numeric(Surface_BV_topo),
            Q2_m3 = as.numeric(Q2_m3),
            )
   
   
-  #filtre Q2 sur la région : 
+  #filtre Q2 sur les sites d'interet: 
   
-  sites_hdf<-info_sites_hydro$code_site #vecteur contenant les codes de sites hydro de la région selectionnée 
+  hydro_carhyce<-hydro_carhyce %>% 
+    mutate(code_site_hydro =substr(code_sta_hydro, 1,8))
+  
+  
+  ## Filtre de Q2_hdf sur les sites d'interet 
+  
+  sites_interet<-unique(hydro_carhyce$code_site_hydro) 
   
   Q2_fr<-Q2_fr %>% 
-    filter(Code_sta %in% sites_hdf) # filtre des Q23 sur ces codes de sites (Attention aux éventuels codes de sites rassemblant plusieurs codes stations)
+    filter(Code_sta %in% sites_interet) # filtre des Q23 sur ces codes de sites (Attention aux éventuels codes de sites rassemblant plusieurs codes stations)
   
   
   # 1ère jointure entre hydro_carhyce et carhyce_hdf_ofb pour récupérer la surface de BV amont aux stations carhyce : 
@@ -37,7 +43,10 @@ function_Qb_Q2 <- function(code_region, Q2_fr, hydro_carhyce){
   carhyce_hdf_ofb_bv<-carhyce_hdf_ofb %>%  #selection des colonnes de code station et surface de BV 
     select(code_sta_HM=Code_sta, surface_bv_km2, Date, Q_plein_bord)
   
-  hydro_carhyce_vf<-merge(carhyce_hdf_ofb_bv, hydro_carhyce, by="code_sta_HM")
+  hydro_carhyce<-hydro_carhyce %>% 
+    mutate(code_sta_HM = as.character(code_sta_HM))
+  
+  hydro_carhyce_vf<-left_join(carhyce_hdf_ofb_bv, hydro_carhyce, by="code_sta_HM")
   
   
   # 2ème jointure entre hydro_carhyce_vf et Q_interet pour récupérer les Q2 et surface de BV amont aux stations hydro : 
@@ -48,7 +57,7 @@ function_Qb_Q2 <- function(code_region, Q2_fr, hydro_carhyce){
   hydro_carhyce_vf<-hydro_carhyce_vf %>% 
     mutate(code_site_hydro =substr(code_sta_hydro, 1,8))
   
-  hydro_carhyce_vf<-merge(hydro_carhyce_vf, 
+  hydro_carhyce_vf<-left_join(hydro_carhyce_vf, 
                           Q2_fr, 
                           by="code_site_hydro") 
   
